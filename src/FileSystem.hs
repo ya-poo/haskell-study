370770bec752bca91a1ea8bf8f41b1f1d822d3d4
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2021-11-04 21:05:05 +0900

    FileSystem

---
+++ src/FileSystem.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module FileSystem where
+
+type Name = String
+type Data = String
+data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
+
+data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
+
+type FSZipper = (FSItem, [FSCrumb])
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, FSCrumb name ls rs:bs) =
+    (Folder name (ls ++ [item] ++ rs), bs)
+
+fsTo :: Name -> FSZipper -> FSZipper
+fsTo name (Folder folderName items, bs) =
+    let (ls, item:rs) = break (nameIs name) items
+    in (item, FSCrumb folderName ls rs:bs)
+
+nameIs :: Name -> FSItem -> Bool 
+nameIs name (Folder folderName _) = name == folderName
+nameIs name (File fileName _) = name == fileName
+
+fsRename :: Name -> FSZipper -> FSZipper
+fsRename newName (Folder name items, bs) = (Folder newName items, bs)
+fsRename newName (File name dat, bs) = (File newName dat, bs)
